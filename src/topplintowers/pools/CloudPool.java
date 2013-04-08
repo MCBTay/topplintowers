fa966f32deefddb1ab1911cfc402927afe0f6511
mcbtay <mcbtay@gmail.com>
2013-04-07 22:16:14 -0400

    removed some commented out code

--- src/topplintowers/pools/CloudPool.java
+++ src/topplintowers/pools/CloudPool.java
@@ -24,11 +24,4 @@ public class CloudPool extends GenericPool<Sprite> {
 	protected void recycle(Sprite cloud) {
 		this.recyclePoolItem(cloud);
 	}
-	
-	/*int randomCloud = (int)((float)Math.random() * 6);	
-	TextureRegion cloudTexture = ResourceManager.mCloudTextureRegions.get(randomCloud);
-	
-	//TODO: change this to use a sprite pool!
-	Sprite newCloud = new Sprite(0, 0, cloudTexture, vbom);*/
-
 }
