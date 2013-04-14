fbbccde9bfb5a2122ef54481088174842aa51ab0
mcbtay <mcbtay@gmail.com>
2013-04-13 20:05:20 -0400

    removed unused imports

--- src/topplintowers/crates/StoneCrate.java
+++ src/topplintowers/crates/StoneCrate.java
@@ -1,15 +1,10 @@
 package topplintowers.crates;
 
-import org.andengine.entity.sprite.Sprite;
 import org.andengine.extension.physics.box2d.PhysicsConnector;
 import org.andengine.extension.physics.box2d.PhysicsFactory;
-import org.andengine.opengl.texture.region.TextureRegion;
-
 import com.badlogic.gdx.physics.box2d.BodyDef.BodyType;
 import com.badlogic.gdx.physics.box2d.FixtureDef;
 
-import topplintowers.MainActivity;
-import topplintowers.ResourceManager;
 import topplintowers.pools.PoolManager;
 import topplintowers.scenes.GameScene;
 
